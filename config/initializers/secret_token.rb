9510ab7b83a88483fb629ef8bf168f1792e3846a
Will Sommers <will.sommers@gmail.com>
2014-04-06 11:02:40 -0400

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ptucker::Application.config.secret_key_base = '9ad91e406af4cf36047eda79745dd4d1d87b8ce166a313ea215688cb4757e88217442b0cecf5e08ca1d02b0a1f2ab8f1992401a4ceb3521f8e305c5d557060e2'
