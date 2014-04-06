2d17d595584e41a4c71eff43e5cb2d40d280e1d0
Will Sommers <will.sommers@gmail.com>
2014-04-06 12:47:09 -0400

    Adding seed categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+#
+
+
+categories = ["hollowware", "flatware", "novelties", "boxes", "purses", "jewelry"]
+
+categories.each do |c|
+  Category.create(name: c, display_name: c.capitalize)
+end
