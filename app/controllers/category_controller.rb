1bd021dfbda053452c27c7dedac6ae57c875170a
Will Sommers <will.sommers@gmail.com>
2014-04-06 13:02:44 -0400

    HI HANK

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -1,2 +1,6 @@
 class CategoryController < ApplicationController
+
+  def show
+    @id = params[:id]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Ptucker::Application.routes.draw do
   resources :products
 
+  get "categories/:id", to: "category#show"
+
   get "/about", to: "static#about"
   root "static#index"
 end
